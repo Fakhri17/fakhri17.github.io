9b75fbe1b389f767f232d0ed0ca631ca9f1e3b68
Fakhri17 <fakhricoffe@gmail.com>
2022-05-06 23:06:33 +0700

    adding stack tech

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,35 @@
- // On page load or when changing themes, best to add inline in `head` to avoid FOUC
- if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
-     '(prefers-color-scheme: dark)').matches)) {
-   document.documentElement.classList.add('dark');
- } else {
-   document.documentElement.classList.remove('dark')
- }
- var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
- var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+function alpine_tabs() {
+  return {
+    selected: 'about',
+    darkMode: false,
+    init() {
+      var lazyLoadInstance = new LazyLoad({
+        // Your custom settings go here
+      });
+      if (localStorage.theme === 'dark') {
+        document.querySelector('html').classList.add('dark');
+        document.querySelector('html').classList.remove('light');
+      } else {
+        document.querySelector('html').classList.remove('dark');
+        document.querySelector('html').classList.add('light');
+      }
+      this.darkMode = document.querySelector('html').classList.contains('dark')
+    },
 
- // Change the icons inside the button based on previous settings
- if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
-     '(prefers-color-scheme: dark)').matches)) {
-   themeToggleLightIcon.classList.remove('hidden');
- } else {
-   themeToggleDarkIcon.classList.remove('hidden');
- }
-
- var themeToggleBtn = document.getElementById('theme-toggle');
-
- themeToggleBtn.addEventListener('click', function () {
-
-   // toggle icons inside button
-   themeToggleDarkIcon.classList.toggle('hidden');
-   themeToggleLightIcon.classList.toggle('hidden');
-
-   // if set via local storage previously
-   if (localStorage.getItem('color-theme')) {
-     if (localStorage.getItem('color-theme') === 'light') {
-       document.documentElement.classList.add('dark');
-       localStorage.setItem('color-theme', 'dark');
-     } else {
-       document.documentElement.classList.remove('dark');
-       localStorage.setItem('color-theme', 'light');
-     }
-
-     // if NOT set via local storage previously
-   } else {
-     if (document.documentElement.classList.contains('dark')) {
-       document.documentElement.classList.remove('dark');
-       localStorage.setItem('color-theme', 'light');
-     } else {
-       document.documentElement.classList.add('dark');
-       localStorage.setItem('color-theme', 'dark');
-     }
-   }
-
- });
+    change_dark() {
+      if (localStorage.theme === 'dark') {
+        localStorage.theme = 'light';
+        document.querySelector('html').classList.add('light');
+        document.querySelector('html').classList.remove('dark');
+      } else {
+        localStorage.theme = 'dark';
+        document.querySelector('html').classList.add('dark');
+        document.querySelector('html').classList.remove('light');
+      }
+      this.darkMode = document.querySelector('html').classList.contains('dark')
+    },
+    button_tabs(data) {
+      this.selected = data
+    },
+  }
+}
